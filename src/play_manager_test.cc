cca09f2411488ab8845afbb6ff64f7d1a4efa484
Brendan Hansknecht <brendan.hansknecht@gmail.com>
2021-04-17 18:54:06 -0700

    Add PlayManager multithreaded test

--- src/play_manager_test.cc
+++ src/play_manager_test.cc
@@ -12,8 +12,8 @@ namespace {
 TEST(PlayManager, Basic) {
   auto params = PlayParams{};
   params.base_gs = std::make_unique<connect4_gs::Connect4GS>();
-  params.games_to_play = 128;
-  params.concurrent_games = 16;
+  params.games_to_play = 32;
+  params.concurrent_games = 8;
   auto pm = PlayManager{std::move(params)};
   auto play = std::async(std::launch::async, [&] {
     try {
@@ -30,6 +30,39 @@ TEST(PlayManager, Basic) {
     }
   });
   play.wait();
+  infer.wait();
+}
+
+TEST(PlayManager, MultiThreaded) {
+  const auto cores = std::thread::hardware_concurrency();
+  const auto workers = cores - 1;
+
+  auto params = PlayParams{};
+  params.base_gs = std::make_unique<connect4_gs::Connect4GS>();
+  params.games_to_play = 32 * workers;
+  params.concurrent_games = 8 * workers;
+
+  auto pm = PlayManager{std::move(params)};
+  auto play_workers = std::vector<std::future<void>>{workers};
+  for (auto& pw : play_workers) {
+    pw = std::async(std::launch::async, [&] {
+      try {
+        pm.play();
+      } catch (const std::exception& e) {
+        FAIL() << "Got an exception: " << e.what() << std::endl;
+      }
+    });
+  }
+  auto infer = std::async(std::launch::async, [&] {
+    try {
+      pm.dumb_inference();
+    } catch (const std::exception& e) {
+      FAIL() << "Got an exception: " << e.what() << std::endl;
+    }
+  });
+  for (auto& pw : play_workers) {
+    pw.wait();
+  }
   infer.wait();
 }
 

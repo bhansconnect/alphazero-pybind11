7ed79c09b8114d7726dde3d585ecef22904424a1
Brendan Hansknecht <brendan.hansknecht@gmail.com>
2021-04-18 00:14:28 -0700

    Add batch size to simulate nn with delay

--- src/dumb_eval_runner.py
+++ src/dumb_eval_runner.py
@@ -18,33 +18,43 @@ spec.loader.exec_module(alphazero)
 Game = alphazero.Connect4GS
 
 
-def dumb_eval(pm):
+def dumb_eval(pm, bs):
     while(pm.remaining_games() > 0):
-        i = pm.pop_game()
-        if i is None:
-            continue
-        gd = pm.game_data(i)
-        gd.v().fill(0)
-        pi = gd.pi()
-        pi[:] = gd.valid_moves()
-        total = np.sum(pi)
-        if total != 0:
-            pi /= total
-        pm.push_inference(i)
+        batch = []
+        start = time.time()
+        while len(batch) < min(bs, pm.remaining_games()):
+            i = pm.pop_game()
+            if i is None:
+                continue
+            batch.append(i)
+            gd = pm.game_data(i)
+            gd.v().fill(0)
+            pi = gd.pi()
+            pi[:] = gd.valid_moves()
+            total = np.sum(pi)
+            if total != 0:
+                pi /= total
+        elapsed = time.time() - start
+        remaining = 0.059 - elapsed
+        if remaining > 0:
+            time.sleep(remaining)
+        for i in batch:
+            pm.push_inference(i)
 
 
 if __name__ == '__main__':
     procs = os.cpu_count()
     workers = procs - 1
     params = alphazero.PlayParams()
-    n = 128*workers
+    n = 512*12
+    bs = 512
     params.games_to_play = n
-    params.concurrent_games = 16 * workers
-    params.mcts_depth = 100
+    params.concurrent_games = bs * 2
+    params.mcts_depth = 50
 
     pm = alphazero.PlayManager(alphazero.Connect4GS(), params)
 
-    inference = threading.Thread(target=dumb_eval, args=(pm,))
+    inference = threading.Thread(target=dumb_eval, args=(pm, bs, ))
     inference.start()
     start = time.time()
     with ThreadPool(workers) as pool:

77b47fb318365cb03db60f8f167170595dbe5b20
Brendan Hansknecht <brendan.hansknecht@gmail.com>
2021-04-27 11:52:08 -0700

    Update melo calculation and save to file

--- src/calculate_melo.py
+++ src/calculate_melo.py
@@ -13,21 +13,12 @@ if __name__ == '__main__':
 
     size = wr.shape[0]
     agents = set()
-    agent_to_index = {}
+    matchups = set()
     for i in range(wr.shape[0]):
         for j in range(wr.shape[0]):
             if not math.isnan(wr[i, j]):
                 agents.add(i)
-                agent_to_index[len(agents)-1] = i
-                break
-
-    matchups = set()
-    for Ai in range(len(agents)):
-        for Aj in range(Ai+1, len(agents)):
-            i = agent_to_index[Ai]
-            j = agent_to_index[Aj]
-            if not math.isnan(wr[i, j]):
-                matchups.add((Ai, Aj))
+                matchups.add((i, j))
 
     alpha = math.log(10)/400
 
@@ -45,8 +36,8 @@ if __name__ == '__main__':
         ]
         return r_update, c_update
 
-    melo = np.zeros(len(agents))
-    c = np.random.uniform(-1, 1, (len(agents), 2))
+    melo = np.zeros(wr.shape[0])
+    c = np.random.uniform(-1, 1, (wr.shape[0], 2))
     rounds = 10000
     with tqdm.tqdm(range(rounds)) as pbar:
         for it in pbar:
@@ -54,17 +45,17 @@ if __name__ == '__main__':
                 print(flush=True)
             change_elo = np.zeros_like(melo)
             change_c = np.zeros_like(c)
-            for (Ai, Aj) in matchups:
+            for (i, j) in matchups:
                 melo_update, c_update = melo2_update(
-                    1, Ai, Aj, wr[agent_to_index[Ai], agent_to_index[Aj]], melo, c)
-                melo[Ai] += melo_update[0]
-                melo[Aj] += melo_update[1]
-                c[Ai] += c_update[0]
-                c[Aj] += c_update[1]
-                change_elo[Ai] += np.abs(melo_update[0])
-                change_elo[Aj] += np.abs(melo_update[1])
-                change_c[Ai] += np.abs(c_update[0])
-                change_c[Aj] += np.abs(c_update[1])
+                    1, i, j, wr[i, j], melo, c)
+                melo[i] += melo_update[0]
+                melo[j] += melo_update[1]
+                c[i] += c_update[0]
+                c[j] += c_update[1]
+                change_elo[i] += np.abs(melo_update[0])
+                change_elo[j] += np.abs(melo_update[1])
+                change_c[i] += np.abs(c_update[0])
+                change_c[j] += np.abs(c_update[1])
             # Weight decay of c. This makes the algorithm converge to elo if their are no cycles
             c *= 0.995
             if it % 100 == 0:
@@ -72,8 +63,15 @@ if __name__ == '__main__':
                                  c_cost=np.average(change_c))
 
     melo -= melo[0]
-    print(melo)
-    print(c)
+    np.savetxt("data/melo.csv", melo, delimiter=",")
+    tmp_melo = np.zeros(len(agents))
+    tmp_c = np.zeros((len(agents),2))
+    tmp_melo[:len(agents)-1] = melo[:len(agents)-1]
+    tmp_melo[len(agents)-1] = melo[wr.shape[0]-1]
+    tmp_c[:len(agents)-1] = c[:len(agents)-1]
+    tmp_c[len(agents)-1] = c[wr.shape[0]-1]
+    print(tmp_c)
+    print(tmp_melo)
 
     fig, ax1 = plt.subplots()
     ax1.plot(sorted(agents)[:len(agents)-1],

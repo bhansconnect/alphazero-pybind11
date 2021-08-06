a79acc76006d6a63426e7de13ac3c0f1e0f4146c
Brendan Hansknecht <brendan.hansknecht@gmail.com>
2021-08-06 10:10:23 -0700

    Fix naming bug

--- src/neural_net.py
+++ src/neural_net.py
@@ -177,7 +177,7 @@ class NNWrapper:
         loss = np.zeros(size)
         self.nnet.eval()
         i = 0
-        for batch in tqdm.tqdm(dataset, desc='Calculating Sample Loss', leave=False):
+        for batch in tqdm(dataset, desc='Calculating Sample Loss', leave=False):
             canonical, target_vs, target_pis = batch
             if self.cuda:
                 canonical = canonical.contiguous().cuda()

57204eab2b5e8fb69931ee270d3d0adc16b1cf5e
Brendan Hansknecht <brendan.hansknecht@gmail.com>
2022-06-28 23:05:51 -0700

    switch play bench to tawlbwrdd

--- src/play_manager_bench.cc
+++ src/play_manager_bench.cc
@@ -2,20 +2,20 @@
 
 #include <future>
 
-#include "connect4_gs.h"
 #include "play_manager.h"
+#include "tawlbwrdd_gs.h"
 
 namespace alphazero {
 namespace {
 
 void PlayGameSingleThreaded() {
   auto params = PlayParams{};
-  params.games_to_play = 512;
-  params.concurrent_games = 512;
-  params.mcts_depth = {450, 450};
+  params.games_to_play = 64;
+  params.concurrent_games = 64;
+  params.mcts_depth = {250, 250};
   params.history_enabled = true;
   params.playout_cap_randomization = true;
-  auto pm = PlayManager{std::make_unique<connect4_gs::Connect4GS>(), params};
+  auto pm = PlayManager{std::make_unique<tawlbwrdd_gs::TawlbwrddGS>(), params};
   auto play = std::async(std::launch::async, [&] { pm.play(); });
   auto infer_p0 = std::async(std::launch::async, [&] { pm.dumb_inference(0); });
   auto infer_p1 = std::async(std::launch::async, [&] { pm.dumb_inference(1); });
@@ -29,12 +29,12 @@ void PlayGameMultiThreaded() {
   const auto workers = cores - 1;
 
   auto params = PlayParams{};
-  params.games_to_play = 512;
-  params.concurrent_games = 512;
-  params.mcts_depth = {450, 450};
+  params.games_to_play = 64;
+  params.concurrent_games = 64;
+  params.mcts_depth = {250, 250};
   params.history_enabled = true;
   params.playout_cap_randomization = true;
-  auto pm = PlayManager{std::make_unique<connect4_gs::Connect4GS>(), params};
+  auto pm = PlayManager{std::make_unique<tawlbwrdd_gs::TawlbwrddGS>(), params};
   auto play_workers = std::vector<std::future<void>>{workers};
   for (auto& pw : play_workers) {
     pw = std::async(std::launch::async, [&] { pm.play(); });

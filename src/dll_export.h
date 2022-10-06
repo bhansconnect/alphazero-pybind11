aa1657733d3f817623502ec1f8597571c52ba8c7
Brendan Hansknecht <brendan.hansknecht@gmail.com>
2022-10-06 16:31:23 -0700

    more fighting with msvc

--- src/dll_export.h
+++ src/dll_export.h
@@ -1,9 +1,14 @@
+#pragma once
+
 #if defined _WIN32 || defined __CYGWIN__
 #ifdef BUILDLIB
 #define DLLEXPORT __declspec(dllexport)
+#define WEAKDLLEXPORT __declspec(dllexport)
 #else
 #define DLLEXPORT __declspec(dllimport)
+#define WEAKDLLEXPORT
 #endif
 #else
 #define DLLEXPORT
+#define WEAKDLLEXPORT
 #endif

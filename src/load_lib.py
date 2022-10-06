8a5dac184a9f9b8c4206577cff5c0673d2502e78
Brendan Hansknecht <brendan.hansknecht@gmail.com>
2022-10-06 15:26:05 -0700

    a bit more msvc cleanup

--- src/load_lib.py
+++ src/load_lib.py
@@ -6,10 +6,12 @@ import os
 def load_alphazero():
     src_path = os.path.dirname(os.path.realpath(__file__))
     build_path = os.path.join(os.path.dirname(src_path), 'build/src')
-    lib_path = glob.glob(os.path.join(build_path, 'alphazero*.so'))[0]
+    so_path = glob.glob(os.path.join(build_path, 'alphazero*.so'))
+    lib_path = glob.glob(os.path.join(build_path, 'alphazero*.lib'))
+    final_path = (lib_path + so_path)[0]
 
     spec = importlib.util.spec_from_file_location(
-        'alphazero', lib_path)
+        'alphazero', final_path)
     alphazero = importlib.util.module_from_spec(spec)
     spec.loader.exec_module(alphazero)
     return alphazero

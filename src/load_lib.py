6ad9a635746e4353489e495d2ecf92433b1b16c4
Brendan Hansknecht <brendan.hansknecht@gmail.com>
2022-10-06 16:50:43 -0700

    Misc cleanup

--- src/load_lib.py
+++ src/load_lib.py
@@ -7,11 +7,11 @@ def load_alphazero():
     src_path = os.path.dirname(os.path.realpath(__file__))
     build_path = os.path.join(os.path.dirname(src_path), 'build/src')
     so_path = glob.glob(os.path.join(build_path, 'alphazero*.so'))
-    lib_path = glob.glob(os.path.join(build_path, 'alphazero*.lib'))
-    final_path = (lib_path + so_path)[0]
+    pyd_path = glob.glob(os.path.join(build_path, 'alphazero*.pyd'))
+    lib_path = (pyd_path + so_path)[0]
 
     spec = importlib.util.spec_from_file_location(
-        'alphazero', final_path)
+        'alphazero', lib_path)
     alphazero = importlib.util.module_from_spec(spec)
     spec.loader.exec_module(alphazero)
     return alphazero

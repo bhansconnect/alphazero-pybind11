dbb96adf177a1f2bd5d331bb28d877673f412ecb
Brendan Hansknecht <brendan.hansknecht@gmail.com>
2022-10-27 15:51:37 -0700

    add sharded lru cache

--- src/lru_cache.h
+++ src/lru_cache.h
@@ -6,6 +6,7 @@
 #include <optional>
 
 #include "absl/container/flat_hash_map.h"
+#include "absl/hash/hash.h"
 
 namespace alphazero {
 
@@ -47,7 +48,7 @@ class LRUCache {
 
   void insert_many(const std::vector<K>& ks, const std::vector<V>& vs) {
     std::unique_lock l{m_};
-    for (auto i = 0; i < ks.size(); ++i) {
+    for (size_t i = 0; i < ks.size(); ++i) {
       const auto k = ks[i];
       const auto it = cache_.find(k);
       if (it != cache_.end()) {
@@ -64,9 +65,9 @@ class LRUCache {
     }
   }
 
-  [[nodiscard]] size_t hits() { return hits_; };
-  [[nodiscard]] size_t misses() { return misses_; };
-  [[nodiscard]] size_t size() { return lru_.size(); };
+  [[nodiscard]] size_t hits() const { return hits_; };
+  [[nodiscard]] size_t misses() const { return misses_; };
+  [[nodiscard]] size_t size() const { return lru_.size(); };
 
  private:
   using List = std::list<std::tuple<K, V>>;
@@ -76,6 +77,58 @@ class LRUCache {
   size_t ms_;
   size_t hits_ = 0;
   size_t misses_ = 0;
+};
+
+template <typename K, typename V>
+class ShardedLRUCache {
+ public:
+  ShardedLRUCache(size_t max_size, size_t shards = 1) {
+    for (size_t i = 0; i < shards; ++i) {
+      caches_.push_back(std::make_unique<LRUCache<K, V>>(max_size / shards));
+    }
+  }
+
+  std::optional<V> find(const K k) { return get_cache(k)->find(k); }
+
+  void insert(const K& k, const V& v) { get_cache(k)->insert(k, v); }
+
+  void insert_many(const std::vector<K>& ks, const std::vector<V>& vs) {
+    for (size_t i = 0; i < ks.size(); ++i) {
+      insert(ks[i], vs[i]);
+    }
+  }
+
+  [[nodiscard]] size_t hits() const {
+    size_t hits = 0;
+    for (const auto& cache : caches_) {
+      hits += cache->hits();
+    }
+    return hits;
+  };
+  [[nodiscard]] size_t misses() const {
+    size_t misses = 0;
+    for (const auto& cache : caches_) {
+      misses += cache->misses();
+    }
+    return misses;
+  };
+  [[nodiscard]] size_t size() const {
+    size_t size = 0;
+    for (const auto& cache : caches_) {
+      size += cache->size();
+    }
+    return size;
+  };
+
+ private:
+  LRUCache<K, V>* get_cache(const K& k) {
+    if (caches_.size() == 1) {
+      return caches_[0].get();
+    }
+    return caches_[absl::HashOf(k) % caches_.size()].get();
+  }
+
+  std::vector<std::unique_ptr<LRUCache<K, V>>> caches_;
 };
 
 }  // namespace alphazero
